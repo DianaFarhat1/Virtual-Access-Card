b6caa2a8509e24186d797a60a4366ef1c1eb8cd7
Diana Farhat <104085032+DianaFarhat1@users.noreply.github.com>
2022-05-20 00:31:21 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public abstract class User {
+	
+	String firstName;
+	String lastName;
+	String middleName;
+	LocalDate birthdate;		
+	Integer idNumber;
+	
+	
+
+	
+	
+
+}
